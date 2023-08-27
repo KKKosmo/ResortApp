1962b94c3f83ff06d803e6403b5c56188ef87616
manil <manilalouisangel2@gmail.com>
2023-08-28 04:46:45 +0800

    after learning about exporting java to exe, and jdk and jre

---
+++ src/main/java/com/resort/resortapp/Main.java
@@ -0,0 +1,11 @@
+package com.resort.resortapp;
+
+//import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args){
+        HelloApplication.main(args);
+//        Application.launch(HelloApplication.class, args);
+        System.out.println("FUCK ME");
+    }
+}
