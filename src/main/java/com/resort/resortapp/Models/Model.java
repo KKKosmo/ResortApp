77a32831efce2b7068acec8f85c3ab536c241d94
manil <manilalouisangel2@gmail.com>
2023-09-18 20:01:33 +0800

    sample pdf done

--- src/main/java/com/resort/resortapp/Models/Model.java
+++ src/main/java/com/resort/resortapp/Models/Model.java
@@ -2,7 +2,11 @@ package com.resort.resortapp.Models;
 
 import com.itextpdf.kernel.geom.PageSize;
 import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.layout.border.Border;
+import com.itextpdf.layout.element.Cell;
 import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Table;
+import com.itextpdf.layout.property.TextAlignment;
 import com.resort.resortapp.Rooms;
 import com.resort.resortapp.Views.ViewFactory;
 import javafx.scene.layout.FlowPane;
@@ -14,6 +18,7 @@ import java.time.ZonedDateTime;
 
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
+
 
 
 public class Model {
@@ -121,7 +126,63 @@ public class Model {
             pdfDocument.setDefaultPageSize(PageSize.LETTER);
             Document document = new Document(pdfDocument);
 
-            document.add(new Paragraph(("pls")));
+            float col1 = 285f;
+            float col2 = 435f;
+            float[] headerWidth = {col1, col2};
+            Table table = new Table(headerWidth);
+            table.addCell(new Cell().add("J&G Resort\n" +
+                    "Monthly Report\n" +
+                    "August 2023\n" +
+                    "Page 1/1").setBold().setTextAlignment(TextAlignment.CENTER));
+
+            Table nestedTable = new Table(new float[]{col1/2, col1/2});
+            nestedTable.addCell(new Cell().add("Total Reservations").setBorder(Border.NO_BORDER).setBold());
+            nestedTable.addCell(new Cell().add("0").setBorder(Border.NO_BORDER));
+            nestedTable.addCell(new Cell().add("Total Guests").setBorder(Border.NO_BORDER).setBold());
+            nestedTable.addCell(new Cell().add("0").setBorder(Border.NO_BORDER));
+            nestedTable.addCell(new Cell().add("Total Revenue").setBorder(Border.NO_BORDER).setBold());
+            nestedTable.addCell(new Cell().add("0 PHP").setBorder(Border.NO_BORDER));
+
+            table.addCell(nestedTable);
+
+            document.add(table);
+
+            Table body = new Table(new float[]{0,0,0,0,0,0,0,0,0,0,0});
+            body.addCell(new Cell().add("ID"));
+            body.addCell(new Cell().add("Date"));
+            body.addCell(new Cell().add("Name"));
+            body.addCell(new Cell().add("No. of Heads"));
+            body.addCell(new Cell().add("Vehicle"));
+            body.addCell(new Cell().add("Pets"));
+            body.addCell(new Cell().add("Videoke"));
+            body.addCell(new Cell().add("Partial Payment"));
+            body.addCell(new Cell().add("Check-In"));
+            body.addCell(new Cell().add("Check-Out"));
+            body.addCell(new Cell().add("Room"));
+            body.addCell(new Cell().add("User"));
+            body.addCell(new Cell().add("test"));
+            body.addCell(new Cell().add("test"));
+            body.addCell(new Cell().add("test"));
+            body.addCell(new Cell().add("test"));
+            body.addCell(new Cell().add("test"));
+            body.addCell(new Cell().add("test"));
+            body.addCell(new Cell().add("test"));
+            body.addCell(new Cell().add("test"));
+            body.addCell(new Cell().add("test"));
+            body.addCell(new Cell().add("test"));
+            body.addCell(new Cell().add("test"));
+            body.addCell(new Cell().add("test"));
+            body.addCell(new Cell().add("test"));
+            body.addCell(new Cell().add("test"));
+            body.addCell(new Cell().add("test"));
+            body.addCell(new Cell().add("test"));
+            body.addCell(new Cell().add("test"));
+            body.addCell(new Cell().add("test"));
+            body.addCell(new Cell().add("test"));
+            body.addCell(new Cell().add("test"));
+            body.addCell(new Cell().add("test"));
+            document.add(new Paragraph("THIS IS A PREVIEW FILE").setBold().setTextAlignment(TextAlignment.CENTER));
+            document.add(body);
             document.close();
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
